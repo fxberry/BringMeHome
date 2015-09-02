f30f360c968ec525308634b88bb8cf9e1bd90f08
felix <fx.egli@gmail.com>
2015-09-02 10:17:46 +0200

    added to the min.references.ts the files which had no interfaces

--- app/min.references.ts
+++ app/min.references.ts
@@ -2,3 +2,6 @@
 /// <reference path="interfaces/interface.references.ts" />
 /// <reference path="injections.ts" />
 /// <reference path="constants.ts" />
+
+/// <reference path="services/logger.ts" />
+/// <reference path="run-services/LocalizationConfig.ts" />
