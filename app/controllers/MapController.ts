9b7e04117fccd3e2d80e62c9e9826ba4579a85f7
felix <fx.egli@gmail.com>
2015-10-22 11:37:55 +0200

    Lösung Map

--- app/controllers/MapController.ts
+++ app/controllers/MapController.ts
@@ -18,36 +18,38 @@ module Controllers {
             $injections.Services.SelectedPedestrianRoute
         ];
 
-        constructor() {
-
-            // TODO: Get the coords from the selected pedestrian route
-            var coords = undefined;
-            // TODO: Set the from and the to with coordinates
-            this.from = undefined;
-            this.to = undefined;
+        constructor(selectedPedestrianRoute: Services.ISelectedPedestrianRoute) {
+            var coords = selectedPedestrianRoute();
+            this.from = new google.maps.LatLng(coords.from.xLatitude, coords.from.yLongitude);
+            this.to = new google.maps.LatLng(coords.to.xLatitude, coords.to.yLongitude);
 
             this.createMap();
-            this.calculateRoute();            
+            this.calculateRoute();
         }
 
-        private createMap(): void {            
-           // TODO: use direction renderer to render the directions
-
-            // TODO: Use this map options
+        private createMap(): void {
+            this.directionsDisplay = new google.maps.DirectionsRenderer();
             var mapOptions = {
                 zoom: 7,
                 center: this.from
             };
-            // TODO: Use to get the map from the DOM document.getElementById('map-canvas')
+            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+            this.directionsDisplay.setMap(map);
         }
 
         private calculateRoute(): void {
-
-            // TODO: use the directions service to calculate the route (use Walking as TravelMode)
-            // TODO: set it on the display
-
+            var request = {
+                origin: this.from,
+                destination: this.to,
+                travelMode: google.maps.TravelMode.WALKING
+            };
+            this.directionsService.route(request, (response, status) => {
+                if (status == google.maps.DirectionsStatus.OK) {
+                    this.directionsDisplay.setDirections(response);
+                }
+            });
         }
-  }
+    }
 
     export class MapControllerRegister {
         constructor($module: angular.IModule) {
