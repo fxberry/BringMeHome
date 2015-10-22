734718a2e5157650ac40e5915f0093e999feb9fe
felix <fx.egli@gmail.com>
2015-10-22 11:17:57 +0200

    Lösung Filter

--- app/filters/DurationFilter.ts
+++ app/filters/DurationFilter.ts
@@ -4,8 +4,18 @@ module Filters {
 
     class DurationFilter {
         filter = (millseconds:number) => {
+            var seconds = Math.floor(millseconds / 1000);
+            var days = Math.floor(seconds / 86400);
+            var hours = Math.floor((seconds % 86400) / 3600);
+            var minutes = Math.floor(((seconds % 86400) % 3600) / 60);
+            var timeString = '';
+            //if(days > 0) timeString += (days > 1) ? (days + " ds ") : (days + " d ");
+            //if(hours > 0) timeString += (hours > 1) ? (hours + " hs ") : (hours + " h ");
+            //if (minutes >= 0) timeString += (minutes > 1) ? (minutes + " mins ") : (minutes + " min ");
 
-            var timeString = "";
+            hours = hours + days * 24;
+            timeString = hours + ":";
+            timeString += (minutes === 0) ? "00" : (minutes < 10) ? "0" + minutes.toString() : minutes.toString();
 
             return timeString;
         };
